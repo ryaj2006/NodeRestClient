0cd96918ef9c13c94e7f7feb2349272670201cf5
R Yang <ray_yang@mail.fresnostate.edu>
2021-05-08 20:33:12 -0700

    adding lab video codes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://192.168.0.1:3002/api/v1/'
 };
--- src/shared-service/http.service.ts
+++ src/shared-service/http.service.ts
@@ -1,14 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Headers } from '@angular/http';
+import { Http, Headers } from '@angular/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
+  apiURL = '';  // http://localhost:3002/api/v1
   constructor(
-
+    private http: Http
   ) {
+    this.apiURL = environment.apiURL;
+  }
+
+  testing() {
+    console.log('from http service testing...')
+  }
+
+  // get('car');
+  async get(path: string) {
+    const resp = await this.http.get(this.apiURL + path, this.headers).toPromise();
+    console.log('resp from http service get() resp: ', resp.json());
+    return resp.json();
+  }
+
+  //post('car', {make: 'Nissan', model: '350z'});
+  async post(path: string, payload: any) {
+    const resp = await this.http.post(this.apiURL + path, payload, this.headers).toPromise();
+    console.log('from http service post() resp: ', resp.json());
+    return resp.json();
+  }
+
+  // put('car/id/1', {make: 'Toyota', model: 'Celica'});
+  async put(path: string, payload: any) {
+    const resp = await this.http.put(this.apiURL + path, payload, this.headers).toPromise();
+    console.log('from http service put() resp: ', resp.json());
+    return resp.json();
+  }
+
+
+  // delete('car/id/1');
+  async delete(path: string) {
+    const resp = await this.http.delete(this.apiURL + path, this.headers).toPromise();
+    console.log('from http service delete()', resp.json)
+    return resp.json();
+  }
+
+  // logout();
+  async logout() {
+    const resp: any = await this.http.get(this.apiURL + 'user/logout', this.headers).toPromise();
+    console.log('from http service logout() ', resp.json());
+    return resp.json();
   }
 
   get headers() {
